b21c9dfa6055ca37f0fbb6b35afe410b4172b46a
Tejas <tejasspatell2@gmail.com>
2025-09-25 14:21:50 +0530

    final commit

---
+++ src/Pokemon.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './index.css'
+import { PokemonCard } from './PokemonCard';
+function Pokemon() {
+
+    const [pokemon, setpokemon] = useState([]);
+    const [loading, setloading] = useState(true);
+    const [error, seterror] = useState(null)
+    const [searchpkm, setsearchpkm] = useState("");
+
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=100";
+
+    const fetchpokemon = async () => {
+        try {
+            const fetchAPI = await fetch(API);
+            const data = await fetchAPI.json();
+            console.log(data);
+
+            const deatiledData = data.results.map(async (currpokemon) => {
+                const res = await fetch(currpokemon.url);
+                const data = await res.json();
+                return data;
+            })
+
+            console.log(deatiledData);
+
+            const detailresp = await Promise.all(deatiledData);
+
+            console.log(detailresp);
+            setpokemon(detailresp);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+            seterror(error);
+            setloading(false);
+        }
+    }
+    useEffect(() => {
+        fetchpokemon();
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="loading-container">
+                <div className="spinner"></div>
+                <h2>Loading, please wait...</h2>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <>
+                <h1>Error : {error.message}</h1>
+            </>
+        )
+    }
+
+    const SearchPokemon = pokemon.filter((currpokem) => currpokem.name.toLowerCase().includes(searchpkm.toLowerCase()));
+
+    return (
+        <>
+            <section className='container'>
+                <header>
+                    <img className='pokeball' src='../public/images/lock.png'></img>
+                    <h1>Pokedx</h1>
+                </header>
+                <div className='pokemon-search'>
+                    <input type="text" value={searchpkm} onChange={(e) => setsearchpkm(e.target.value)} placeholder='Search Pokemon'></input>
+                </div>
+                <div>
+                    <ul className='pokemon-cards'>
+                        {/* {pokemon.map((currpokemon) => { */}
+                        {SearchPokemon.map((currpokemon) => {
+                            return (
+                                <PokemonCard key={currpokemon.id} data={currpokemon}></PokemonCard>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Pokemon;
---
+++ src/PokemonCard.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import './index.css';
+
+const typeColors = {
+  fire: "orangered",
+  water: "#42a5f5",
+  grass: "rgb(23, 255, 38)",
+  electric: "orange",
+  psychic: "#ba68c8",
+  normal: "#a1887f",
+  ground: "#bcaaa4",
+  rock: "#8d6e63",
+  bug: "rgb(144, 255, 40)",
+  ghost: "#9575cd",
+  poison: "#ab47bc",
+  fairy: "#f06292",
+  fighting: "red",
+  dragon: "#7e57c2",
+  ice: "#4dd0e1",
+  dark: "#455a64",
+  steel: "#90a4ae",
+  flying: "#81d4fa",
+};
+
+export function PokemonCard({ data }) {
+    const primaryType = data.types[0].type.name;
+  const highlightStyle = {
+    backgroundColor: typeColors[primaryType] || "#e0f7fa",
+    color: "#fff",
+    borderRadius: "10px",
+    padding: "5px 10px",
+    display: "inline-block",
+    fontWeight: "bold",
+    textTransform: "capitalize",
+  };
+  return (
+    <>
+      <li className="pokemon-card">
+        <figure>
+          <img
+            src={data.sprites.other.dream_world.front_default}
+            className="pokemon-image"
+          ></img>
+        </figure>
+        <h1 className="pokemon-name">{data.name.toUpperCase()}</h1>
+        <div className="pokemon-info pokemon-highlight" style={highlightStyle}>
+          <p>{data.types.map((currelm) => currelm.type.name).join(" ,  ")}</p>
+        </div>
+        <div className="grid-three-cols">
+          <p className="pokemon-info">
+            Height: <span> {data.height} </span>
+          </p>
+          <p className="pokemon-info">
+            Weight: <span> {data.weight}</span>
+          </p>
+          <p className="pokemon-info">
+            speed: <span>{data.stats[5].base_stat}</span>
+          </p>
+        </div>
+        <div className="grid-three-cols">
+          <div className="pokemon-info">
+            <span> Experience:</span>
+            <p>{data.base_experience}</p>
+          </div>
+          <div className="pokemon-info">
+            <span>Attack:</span>
+            <p>{data.stats[1].base_stat}</p>
+          </div>
+          <div className="pokemon-info">
+            <span> Abilities: </span>
+            <p>
+              {data.abilities
+                .map((abilityInfo) => abilityInfo.ability.name)
+                .slice(0, 1)
+                .join(", ")}
+            </p>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+}
